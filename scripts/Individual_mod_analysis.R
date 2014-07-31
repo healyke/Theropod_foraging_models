91f3087cf19a93bdef4ac398a65c1b6c5db521ba
Kevin Healy <healyke@tcd.ie>
2014-07-31 17:27:22 +0100

    Updates on CV, table layout and general updates

--- scripts/Individual_mod_analysis.R
+++ scripts/Individual_mod_analysis.R
@@ -8,7 +8,7 @@ graphics.off()
 ind_data <- read.csv("full_gut_full_comp_master.csv",sep=",", header = T)
 
 #to get average
-enr_day <- as.data.frame(matrix(0, nrow = length(levels(as.factor(ind_data$ind))) , ncol = c(2), dimnames = list(c(),c("mass","energy"))))
+enr_day <- as.data.frame(matrix(0, nrow = length(levels(as.factor(ind_data$ind))) , ncol = c(3), dimnames = list(c(),c("mass","energy","prop"))))
 daily.cost <-  as.data.frame(matrix(0, nrow = length(levels(as.factor(ind_data$mass))) , ncol = c(2),dimnames = list(c(),c("mass","energy"))))
 daily.cost[,1] <- c(100,500,1000,2000,4000,6000,10000)
 daily.cost[,2] <- c(19243.828,84039.563,158562.583,299170.140,564464.120,818316.553,1306521.754) #remember these are in kilo joules
@@ -17,8 +17,9 @@ daily.cost[,2] <- c(19243.828,84039.563,158562.583,299170.140,564464.120,818316.
 #to produce the average energy
 for(i in 1:(length(levels(as.factor(ind_data$ind))))){
 	
-	enr_day[i,1] <- sum(ind_data[ind_data$ind == i, "energy"]) - (length(ind_data[ind_data$ind == i, "energy"])*daily.cost[daily.cost$mass == ind_data[ind_data$ind == i, "mass"][1],"energy"])
-    enr_day[i,2] <- ind_data[ind_data$ind == i, "mass"][1]
+	enr_day[i,1] <- ind_data[ind_data$ind == i, "mass"][1]
+	enr_day[i,2] <- sum(ind_data[ind_data$ind == i, "energy"]) - (length(ind_data[ind_data$ind == i, "energy"])*daily.cost[daily.cost$mass == ind_data[ind_data$ind == i, "mass"][1],"energy"])
+	enr_day[i,3] <- sum(ind_data[ind_data$ind == i, "energy"])/(length(ind_data[ind_data$ind == i, "energy"])*daily.cost[daily.cost$mass == ind_data[ind_data$ind == i, "mass"][1],"energy"])
 	
 }
 
